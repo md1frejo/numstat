88213ff7a93527b0d0b83b4ce20b372f309bbb70
md1frejo <jonas.mb@gmail.com>
2025-08-27 14:20:34 +0200

    fresh deploy

---
+++ _app/immutable/nodes/0.C_vLF7j-.js
@@ -0,0 +1,44 @@
+import"../chunks/Bzak7iHL.js";import{i as y}from"../chunks/fOCUfojL.js";import{ax as $,ar as f,at as l,aq as i,as as v,au as b,aw as g,av as c,F as w,K as j,V as d,ao as u,ay as q}from"../chunks/wVfyLq6-.js";import{I as A,s as x,a as _}from"../chunks/xDpvwEgz.js";import{l as I,s as M,p}from"../chunks/PnVVKfFO.js";function k(){return{bgtop:"bg-lightblueg-800",bgmain:"bg-lightblueg-700",bgcenter:"bg-lightblueg-700",bginnercenter:"bg-lightblueg-900",bglefta:"bg-red-400",bgleftb:"bg-green4g-200",bgleft:"bg-AntiqueWhiteg-800",bgdisp:"bg-AntiqueWhiteg-800",bgbottom:"bg-brown4g-800"}}const z=!0,L=Object.freeze(Object.defineProperty({__proto__:null,load:k,prerender:z},Symbol.toStringTag,{value:"Module"}));function m(a,e){const o=I(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.540.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * ---
+ *
+ * The MIT License (MIT) (for portions derived from Feather)
+ *
+ * Copyright (c) 2013-2023 Cole Bemis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */const t=[["path",{d:"m8 6 4-4 4 4"}],["path",{d:"M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22"}],["path",{d:"m20 22-5-5"}]];A(a,M({name:"merge"},()=>o,{get iconNode(){return t},children:(r,s)=>{var n=$(),h=f(n);x(h,e,"default",{}),l(r,n)},$$slots:{default:!0}}))}var F=i('<section><div class="flex items-center justify-center space-x-4"><!> <p class="text-top1 text-blueg-200">numstats</p> <!></div></section>');function N(a,e){let o=p(e,"bgtop",8,"");var t=F(),r=b(t),s=b(r);m(s,{class:"w-20 h-20 text-blueg-600"});var n=g(s,4);m(n,{class:"w-20 h-20 text-blueg-600"}),c(r),c(t),v(()=>_(t,1,`${o()} py-1 w-[1530px] mx-auto px-1`)),l(a,t)}var O=i('<footer><div class="text-center text-sm">&copy; 2025 You Inc.</div> <p class="text-sm">&copy; 2025 Jonas Fredriksson. All rights reserved.</p> <p class="text-8xl text-purple4g-500">bottom</p></footer>');function P(a,e){let o=p(e,"bgbottom",8,"");var t=O();v(()=>_(t,1,`max-w-cw1 ${o()} mx-auto text-white py-4`)),l(a,t)}var S=i("<!> <!> <!>l",1);function V(a,e){w(e,!1);let o=p(e,"data",8);y();var t=S(),r=f(t);N(r,{get bgtop(){return u(o()),d(()=>o().bgtop)}});var s=g(r,2);x(s,e,"default",{});var n=g(s,2);P(n,{get bgbottom(){return u(o()),d(()=>o().bgbottom)}}),q(),l(a,t),j()}export{V as component,L as universal};
