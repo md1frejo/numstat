90e5eebcbac4ee644f37e9ab93258fe2104251e1
Jonas Fredriksson <jonas.mb@gmail.com>
2025-09-05 10:23:28 +0200

    Update Bottom.svelte

--- src/lib/components/Bottom.svelte
+++ src/lib/components/Bottom.svelte
@@ -7,9 +7,7 @@
 <style>
 </style>
 
-<footer class={`max-w-cw1 ${bgbottom} mx-auto text-white py-4`}>
-    <div class="text-center text-sm">&copy; 2025 You Inc.</div>
-    <p class="text-sm">&copy; 2025 Jonas Fredriksson. All rights reserved.</p>
-    <p class="text-8xl text-purple4g-500">bottom</p>
+<footer class={`bg-blueg-900 max-w-cw1 ${bgbottom} mx-auto text-white py-4`}>
+    <div class="text-center text-sm">&copy; jonas.mb [at] gmail.com</div>
   </footer>
 
